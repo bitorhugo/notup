758c4f4f9ca8f56b75d55d6566217bf1a1c2062b
bitorhugo <vitorhugosantos144@gmail.com>
2023-02-26 05:16:09 +0000

    first commit

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# check if first timestamp exists
+FILE=$(pwd)/timestamp
+if [[ ! -f "${FILE}" ]]; then
+    date +%s >> ${FILE}
+fi
+
+TIMESTAMP=$(cat ${FILE})
+WEEKFROMNOW=$((${TIMESTAMP} + 604800))
+CURRTIMESTAMP=$(date +%s)
+
+# check if one week has passed
+if [[ ${WEEKFROMNOW} -le ${CURRTIMESTAMP} ]]; then
+    # alert for update/upgrade
+    echo "A week has passed, Update your system.."
+    echo "Run pacman -Syu ? [y/N]"
+fi
